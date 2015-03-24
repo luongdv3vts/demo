a6cfd497e3cf86c7bb116315b628badbd58b4cd3
Duong Van Luong <luongdv3@Duongs-MacBook-Pro.local>
2015-03-24 21:40:13 +0700

    Initial Commit

---
+++ demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo
+//
+//  Created by Duong Van Luong on 3/24/15.
+//  Copyright (c) 2015 Duong Van Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
